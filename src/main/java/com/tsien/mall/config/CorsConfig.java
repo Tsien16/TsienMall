b4c9edce1c37f9a2c886aaf97107c3780e075b83
tsien16 <tsien16@qq.com>
2019-07-30 15:52:05 +0800

    cors support

---
+++ src/main/java/com/tsien/mall/config/CorsConfig.java
@@ -0,0 +1,35 @@
+package com.tsien.mall.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author tsien
+ * @version 1.0.0
+ * @date 2019/7/30 0030 15:35
+ */
+
+public class CorsConfig {
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        // 1允许任何域名使用
+        corsConfiguration.addAllowedOrigin("*");
+        // 2允许任何头
+        corsConfiguration.addAllowedHeader("*");
+        // 3允许任何方法（post、get等）
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+}
